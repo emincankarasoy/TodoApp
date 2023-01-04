551142b878060b7787c3d8b63a122b6b6449226d
Emincan Karasoy <emincan.karasoy@outlook.com>
2023-01-05 02:09:50 +0300

    Configure DB Context for PostgreSQL

---
+++ src/Infrastructure/TodoApp.Persistance/Contexts/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Persistance.Contexts
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions options) : base(options) { }
+        
+    }
+}
---
+++ src/Infrastructure/TodoApp.Persistance/ServiceRegister.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Persistance.Contexts;
+
+namespace TodoApp.Persistance
+{
+    public static class ServiceRegister
+    {
+        public static void RegisterPersistanceServices(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddDbContext<AppDBContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
+        }
+    }
+}
